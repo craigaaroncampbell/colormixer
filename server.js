62e9d39c0a08b1bc1c4bd2c64061c444a71aa606
Craig <cragaaroncampbell@gmail.com>
2017-02-17 10:51:15 -0800

    fix typo consolelog

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const PORT = process.env.port || 3000;
 express().use(express.static(__dirname + '/'))
 .listen(PORT, () => process.stdout.write('up on 3000'));
-nsole.log('up on port: 3000');
+console.log('up on port: 3000');
 });
