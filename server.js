b6d6b83899d0449dc971da801a2f215cb42d8b5b
Craig <cragaaroncampbell@gmail.com>
2017-02-16 22:36:43 -0800

    add process.env.port as port option and modify npm start script

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-
+const PORT = process.env.port || 3000;
 app.use(express.static(__dirname + '/build'));
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log('up on port: 3000');
 });
