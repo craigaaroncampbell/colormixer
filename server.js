dcdf2c6f92ee03cc8cf36923bbf4aef4832f8bea
Craig <cragaaroncampbell@gmail.com>
2017-02-17 11:04:28 -0800

    revert to serve static from build

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const PORT = process.env.port || 3000;
-express().use(express.static(__dirname + '/'))
+express().use(express.static(__dirname + '/build'))
 .listen(PORT, () => process.stdout.write('up on 3000'));
